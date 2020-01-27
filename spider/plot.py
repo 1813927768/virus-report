5a7a8ec93cff350c3a338541f7e17f987330d836
1813927768 <1813927768@qq.com>
2020-01-27 23:21:02 +0800

    make x axies labels sparse enough to be clear

--- spider/plot.py
+++ spider/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plot
+import matplotlib.ticker as ticker
 from utils import loadHistory
 
 colorStyle = {
@@ -32,6 +33,25 @@ ch2en = {
 def getLast(list):
     return list[len(list)-1]
 
+# 防止x轴label过于密集（label最大数目7）
+def adjustXAxies(xArray):
+    maxLabelNum = 7
+    
+    def replaceInterval(xArray,interval):
+        # 防止plot函数自动合并相同的x值（""）
+        resArray = [" "*i for i in range(len(xArray))]
+        for i in range(maxLabelNum):
+            loc = i*(interval+1)
+            if loc < len(xArray):
+                resArray[loc] = xArray[loc]
+        return resArray
+            
+    if len(xArray) <= 7:
+        return xArray
+    else:
+        times = len(xArray)//maxLabelNum
+        return replaceInterval(xArray,times)
+
 def makePlot(x,y,title="national",ignore=['治愈','死亡','疑似']):
     if len(x) <= 1:
         return
@@ -40,12 +60,16 @@ def makePlot(x,y,title="national",ignore=['治愈','死亡','疑似']):
     for key in y[0].keys():
         if key in ignore:
             continue
-        plot.plot(x, list(map(lambda x:x[key],y)), color=colorStyle[key], marker=markerStyle[key], linestyle=lineStyle[key],label=ch2en[key])
+        plot.plot(adjustXAxies(x), list(map(lambda x:x[key],y)), color=colorStyle[key], marker=markerStyle[key], linestyle=lineStyle[key],label=ch2en[key])
+    # plot.xticks(range(1,len(x)+1),)
     plot.title('2019-nConv Report')
     # plot.legend(shadow=True,loc="upper left")
     plot.savefig("./spider/image/2019-nConv_report_%s_%s.jpg"%(title,getLast(x)))
+    plot.close('all')
 
 if __name__ == '__main__':
-    sumList,timeList = loadHistory()
+    sumPath = "./spider/data/sum_nation_nation.json"
+    timePath = "./spider/data/time_nation_nation.json"
+    sumList,timeList = loadHistory(sumPath,timePath)
     makePlot(timeList,sumList)
     
